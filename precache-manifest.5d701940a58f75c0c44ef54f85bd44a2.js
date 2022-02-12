d50ae94c1ea86c0312fa2195cce971649ebc8a3d
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-02-12 12:22:20 +0600

    first commit

---
+++ precache-manifest.5d701940a58f75c0c44ef54f85bd44a2.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c9e5b0fd28fa3319c037eb940a8616",
+    "url": "/portfolio-build/index.html"
+  },
+  {
+    "revision": "fab805c744c0f79af3a3",
+    "url": "/portfolio-build/static/css/main.e7c60bd2.chunk.css"
+  },
+  {
+    "revision": "3dfe11b82298be53146a",
+    "url": "/portfolio-build/static/js/2.7d92adfe.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/portfolio-build/static/js/2.7d92adfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab805c744c0f79af3a3",
+    "url": "/portfolio-build/static/js/main.4f45a8f4.chunk.js"
+  },
+  {
+    "revision": "f3ff8506e6f767d84666",
+    "url": "/portfolio-build/static/js/runtime-main.c6a4abd7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio-build/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "a6740b2a7be4028ce3f205c3ab716bda",
+    "url": "/portfolio-build/static/media/Google-logo.a6740b2a.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfolio-build/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portfolio-build/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfolio-build/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfolio-build/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portfolio-build/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portfolio-build/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-build/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "cf179a3c716621f27636709732cb644e",
+    "url": "/portfolio-build/static/media/UM.cf179a3c.jpg"
+  },
+  {
+    "revision": "db648789322d771833efbd28b7ce6f5b",
+    "url": "/portfolio-build/static/media/acps.db648789.png"
+  },
+  {
+    "revision": "bd9460d81c08009b67055a60e5fa409d",
+    "url": "/portfolio-build/static/media/acps1.bd9460d8.png"
+  },
+  {
+    "revision": "5354118c53be5d62fd3cfd8d30f13b8b",
+    "url": "/portfolio-build/static/media/acpsitc.5354118c.png"
+  },
+  {
+    "revision": "07f34c78c49849ac01602452a95c605f",
+    "url": "/portfolio-build/static/media/bracu.07f34c78.png"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/portfolio-build/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "9dd190bcc002cb18d9dc4f7d7c54ee1f",
+    "url": "/portfolio-build/static/media/cwob.9dd190bc.png"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "/portfolio-build/static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "f8925fd8f600d0d4c71a0b915509313b",
+    "url": "/portfolio-build/static/media/duburi.f8925fd8.png"
+  },
+  {
+    "revision": "bc5882f6beecb10255c8aaceacef5c68",
+    "url": "/portfolio-build/static/media/efi.bc5882f6.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfolio-build/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfolio-build/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/portfolio-build/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfolio-build/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfolio-build/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfolio-build/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/portfolio-build/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfolio-build/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio-build/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio-build/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfolio-build/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfolio-build/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/portfolio-build/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfolio-build/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfolio-build/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "/portfolio-build/static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "/portfolio-build/static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/portfolio-build/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/portfolio-build/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "f7288055334eeee2693e025ee2a8a8ee",
+    "url": "/portfolio-build/static/media/hackathon1.f7288055.svg"
+  },
+  {
+    "revision": "bc983af700da607bead2f0bfc089c811",
+    "url": "/portfolio-build/static/media/ibm.bc983af7.png"
+  },
+  {
+    "revision": "8b33b7d26f133987e5677bfcb1d5d705",
+    "url": "/portfolio-build/static/media/ieee.8b33b7d2.png"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "/portfolio-build/static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "/portfolio-build/static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "/portfolio-build/static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "b7f903d747b0409347c9ee67745dbfa4",
+    "url": "/portfolio-build/static/media/localguide.b7f903d7.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "/portfolio-build/static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "/portfolio-build/static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/portfolio-build/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/portfolio-build/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/portfolio-build/static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "b0ce478d18c6ea97131c0519f4401b83",
+    "url": "/portfolio-build/static/media/previewFile.b0ce478d.jpeg"
+  },
+  {
+    "revision": "954ea06d17f47f2ab41642cb2576f599",
+    "url": "/portfolio-build/static/media/rhyme.954ea06d.png"
+  },
+  {
+    "revision": "7b7a76ef55658f6999ce00c5d55f85a5",
+    "url": "/portfolio-build/static/media/skillenza.7b7a76ef.png"
+  },
+  {
+    "revision": "917446ddb3205b45d521d89679c8b20c",
+    "url": "/portfolio-build/static/media/spsc.917446dd.png"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "/portfolio-build/static/media/wrighter.02fdae43.jpg"
+  }
+]);
